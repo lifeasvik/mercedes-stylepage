e4331834d3a368b6838ad6da584caeef53d70604
Vik Birdi <lifeasvik@gmail.com>
2022-07-13 12:17:22 -0400

    new project added menubar

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import Menubar from "./components/Menubar";
+import Splash from "./components/Splash";
+
+function App() {
+  return (
+    <div className='App'>
+      <div>
+        <Menubar />
+        <Splash />
+      </div>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Menubar.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+function Menubar() {
+    return (
+      <main>
+
+        <div className='menubar'>
+            <ul>
+                <li>2022 GLS</li>
+                <li>VEHICLES</li>
+                <li>BUILD</li>
+                <li>MB CERTIFIED PRE OWNED</li>
+            </ul>
+        </div>
+      </main>
+    )
+  }
+
+  export default Menubar;
